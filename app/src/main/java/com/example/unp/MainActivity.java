e0962f733ca33f5733b462b5f4e0a3cf8a6ac166
GuilhermeFM-20 <guilherme_freire_melo@outlook.com>
2024-05-22 23:19:35 -0300

    Primeiro Commit

---
+++ app/src/main/java/com/example/unp/MainActivity.java
@@ -0,0 +1,37 @@
+package com.example.unp;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    @SuppressLint("MissingInflatedId")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.appbar), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+
+    public void alterarText(View v){
+        System.out.println("entrou");
+        EditText name = (EditText)findViewById(R.id.inputName);
+        TextView txt = (TextView)findViewById(R.id.valor);
+        txt.setText(name.getText());
+    }
+
+}
